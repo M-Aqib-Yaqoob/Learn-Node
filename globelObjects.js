c55844bfce17ed8d1278d90ed2ff35e0d7d63ac8
M-Aqib-Yaqoob <muhammadaqibyaqub.com>
2022-02-06 14:48:30 +0500

    first commit

---
+++ globelObjects.js
@@ -0,0 +1,12 @@
+//Hare are some global objects and function tha can be accessed in anywahre in any file
+
+console.log()//global
+setTimeout()//Used this to call a function after a delay like one second two second etc..
+clearTimeout()
+ 
+setinterval( )//Used to repeatedly call a function after a given delay
+clearInterval()//Used to stop that function from being called repeatrdly
+
+
+
+window  
